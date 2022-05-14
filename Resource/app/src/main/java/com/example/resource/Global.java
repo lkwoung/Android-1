1ba91c429485b65c16b5ba2bb8f8e100395bb5fe
lkwoung <whrjzm123@gmail.com>
2022-05-14 18:18:00 +0900

    resource+global

---
+++ Resource/app/src/main/java/com/example/resource/Global.java
@@ -0,0 +1,14 @@
+package com.example.resource;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Global extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_global);
+    }
+}
